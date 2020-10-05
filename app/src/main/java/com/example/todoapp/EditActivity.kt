cab48d60d64ae3d0c51fdbce63e7ca1e74d7f531
Inese Ciniņa <inese.cinina@gmail.com>
2020-10-05 19:43:21 +0300

    datepicker

--- app/src/main/java/com/example/todoapp/EditActivity.kt
+++ app/src/main/java/com/example/todoapp/EditActivity.kt
@@ -1,28 +1,54 @@
 package com.example.todoapp
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.edit_activity.*
 import kotlinx.android.synthetic.main.item_todo.*
-
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class EditActivity : AppCompatActivity() {
     private var idx: Int = -1
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         idx = intent.getIntExtra("pozicija", -1)
 
         setContentView(R.layout.edit_activity)
+        val editView: EditText  = findViewById(R.id.when_edit)
+        //editView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+        var cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            editView.setText(sdf.format(cal.time))
+        }
+
 
         if (idx != -1) {
             val event = intent.getSerializableExtra("dati") as ToDoEvent
             setEvent(event)
         }
+            editView.setOnClickListener {
+                DatePickerDialog(this@EditActivity, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
 
         go_back.setOnClickListener { returnToMain() }
         save_changes.setOnClickListener { saveChanges() }
